1c3e6324a4bb5e050f537d4d5bc6466549da3a27
BenLeibig <ben.leibig@gmail.com>
2021-03-16 06:12:58 +0000

    fix load balancers

--- load_balancers.tf
+++ load_balancers.tf
@@ -17,13 +17,30 @@ data "aws_lb" "public_alb" {
 data "aws_lb" "private_alb" {
   name = var.private_lb_name
 }
-resource "aws_lb_target_group" "bsc-jsonrpc" {
-  name     = "bsc-jsonpc"
+resource "aws_lb_target_group" "bsc-jsonrpc-public" {
+  name     = "bsc-jsonrpc-public"
   port     = "8545"
   protocol = "HTTP"
   vpc_id   = local.vpc_id
   tags = {
-    name = "graphql"
+    name = "bsc-geth"
+  }
+  health_check { ##TODO figure out a better healthcheck maybe getting data from the monitoring/management ports
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+    interval            = 10
+    path                = "/"
+    port                = 8545
+  }
+}
+resource "aws_lb_target_group" "bsc-jsonrpc-private" {
+  name     = "bsc-jsonrpc-private"
+  port     = "8545"
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  tags = {
+    name = "bsc-geth"
   }
   health_check { ##TODO figure out a better healthcheck maybe getting data from the monitoring/management ports
     healthy_threshold   = 3
@@ -35,11 +52,12 @@ resource "aws_lb_target_group" "bsc-jsonrpc" {
   }
 }
 
+
 ## If so they need their own target groups.
 resource "aws_lb_listener_rule" "bsc-node-public" {
   listener_arn = var.public_lb_https_listener_arn
   action {
-    target_group_arn = aws_lb_target_group.bsc-jsonrpc.arn
+    target_group_arn = aws_lb_target_group.bsc-jsonrpc-public.arn
     type             = "forward"
   }
   condition {
@@ -52,7 +70,7 @@ resource "aws_lb_listener_rule" "bsc-node-public" {
 resource "aws_lb_listener_rule" "bsc-node-private" {
   listener_arn = var.private_lb_https_listener_arn
   action {
-    target_group_arn = aws_lb_target_group.bsc-jsonrpc.arn
+    target_group_arn = aws_lb_target_group.bsc-jsonrpc-private.arn
     type             = "forward"
   }
   condition {
